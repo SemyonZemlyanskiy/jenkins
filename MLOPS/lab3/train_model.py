cf745d1bb286488d1057c6b34f18a93eeff341cd
Semyon <semyonzemlyanskiy@gmail.com>
2025-05-01 17:52:02 +0500

    add file

--- MLOPS/lab3/train_model.py
+++ MLOPS/lab3/train_model.py
@@ -1,4 +1,3 @@
-
 from os import name
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import pandas as pd
@@ -79,4 +78,5 @@ if __name__ == "__main__":
 
     dfruns = mlflow.search_runs()
     path2model = dfruns.sort_values("metrics.r2", ascending=False).iloc[0]['artifact_uri'].replace("file://","") + '/model' #путь до эксперимента с лучшей моделью
-    print(path2model)
+    with open('best_model.txt', 'w') as f:
+        f.write(path2model)
